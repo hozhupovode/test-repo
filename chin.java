0e411df047436046975583ca0df5428ce2efa99e
hozhupovode <hozhu@yandex.ru>
2016-03-20 10:16:35 +0400

    создан код

---
+++ chin.java
@@ -0,0 +1,28 @@
+class chin
+{
+public static void main(String[] args)
+throws java.io.IOException 
+	{
+char input;
+int counter = 0;
+System.out.println("Enter symbols, end yor input with dot:\n");
+	do
+		{
+		input = (char) System.in.read();
+		if ((input>=65) && (input<=90))
+			{
+			input += 32;
+			counter++;
+			}
+		else 
+			if ((input>=97) && (input<=122))
+				{
+				input -= 32;
+				counter++;
+				}
+		System.out.println("Your shifted symbol" + input + '\n');
+		}
+	while (input != '.');
+System.out.println("Number of shifted symbols:" + counter);
+	}
+}
